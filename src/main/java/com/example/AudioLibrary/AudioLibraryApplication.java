fcb206181df4458d07494364bd8e3e46fef4f9ae
Korpatiy <tretiakovslava@gmail.com>
2021-05-22 14:13:09 +0500

    Init project

---
+++ src/main/java/com/example/AudioLibrary/AudioLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.AudioLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AudioLibrary/AudioLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AudioLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
